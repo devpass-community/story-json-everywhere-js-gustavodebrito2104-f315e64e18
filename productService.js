b94053cdbbca473989195c1e7d40590840ef2149
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2023-05-03 20:52:21 -0300

    feat: request api response with fetch api

--- productService.js
+++ productService.js
@@ -1,7 +1,10 @@
 async function getProduct(productId) {
     const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
-    // Add your solution here!
+    const response = await fetch(apiUrl);
+    const responseJson = await response.json();
+
+    return responseJson;
 }
 
 module.exports = getProduct;
